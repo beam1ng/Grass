102b2f2cbbfd6757550cdec8a59fdbdafd9498fb
Jakub Augustyniak <jakub.augustyniak1.618@gmail.com>
2024-10-20 03:32:34 +0200

    Removed temporary code.

--- Assets/Solutions/4_RendererFeatureSideIndirectInstancing/Scripts/RendererFeature4.cs
+++ Assets/Solutions/4_RendererFeatureSideIndirectInstancing/Scripts/RendererFeature4.cs
@@ -48,41 +48,38 @@ namespace Solutions._4_RendererFeatureSideIndirectInstancing.Scripts
             this.renderPassEvent = renderPassEvent;
 
             argsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments,5, sizeof(uint));
+            uint[] args = new uint[5] { elevatedQuad.GetIndexCount(0), 100000, 0, 0, 0 };
+            argsBuffer.SetData(args);
         }
 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
         {
-            using (IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass("Grass", out PassData passData))
+            using IRasterRenderGraphBuilder builder = renderGraph.AddRasterRenderPass("Grass", out PassData _);
+            UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+
+            builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
+            builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture);
+
+            builder.UseBuffer(renderGraph.ImportBuffer(argsBuffer));
+            builder.UseBuffer(renderGraph.ImportBuffer(GrassRenderer.AllGrassRenderers[0].PositionBuffer));
+
+            builder.SetRenderFunc((PassData _, RasterGraphContext context) =>
             {
-                UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
-
-                builder.SetRenderAttachment(resourceData.activeColorTexture, 0);
-                builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture);
-
-                builder.UseBuffer(renderGraph.ImportBuffer(argsBuffer));
-                builder.UseBuffer(renderGraph.ImportBuffer(GrassRenderer.AllGrassRenderers[0].PositionBuffer));
-
-                builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
+                foreach (GrassRenderer renderer in GrassRenderer.AllGrassRenderers)
                 {
-                    foreach (GrassRenderer renderer in GrassRenderer.AllGrassRenderers)
-                    {
-                        uint[] args = new uint[5] { elevatedQuad.GetIndexCount(0), 100000, 0, 0, 0 };
-                        argsBuffer.SetData(args);
-                        renderer.GrassMaterial.SetBuffer("_PositionBuffer",renderer.PositionBuffer);
-                        context.cmd.DrawMeshInstancedIndirect(elevatedQuad, 0, renderer.GrassMaterial, 0,
-                            argsBuffer);
-                    }
-                });
-            }
+                    context.cmd.DrawMeshInstancedIndirect(elevatedQuad, 0, renderer.GrassMaterial, 0,
+                        argsBuffer);
+                }
+            });
         }
 
         public void Dispose()
         {
-            // if (argsBuffer != null)
-            // {
-            //     argsBuffer.Dispose();
-            //     argsBuffer = null;
-            // }
+            if (argsBuffer != null)
+            {
+                argsBuffer.Dispose();
+                argsBuffer = null;
+            }
         }
     }
 }
